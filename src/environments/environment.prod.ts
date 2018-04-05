91201aed879870d72c575b024c832141448e3522
Felix <wufeiyue@WUFEIYUEdeMacBook-Air.local>
2018-04-04 20:52:58 -0700

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  baseUrl : 'http://webdev-wu-feiyue.herokuapp.com'
+  baseUrl : 'https://webdev-wu-feiyue.herokuapp.com'
   // baseUrl : 'http://localhost:3100'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // baseUrl : 'http://localhost:3100'
-  baseUrl : 'http://webdev-wu-feiyue.herokuapp.com'
+  baseUrl : 'https://webdev-wu-feiyue.herokuapp.com'
 };
